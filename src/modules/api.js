a317a1d410502a80f51729d5a53864fef3b3b07e
Sigmabond01 <smdnoor4966@gmail.com>
2025-06-13 11:15:24 +0530

    updated

---
+++ src/modules/api.js
@@ -0,0 +1,47 @@
+import { instantiateUIChanges, instantiate404Prompt } from "./ui";
+
+async function getWeatherData(latitude, longitude) {
+  try {
+    const response = await fetch(
+      `https://weatherbond-backend.vercel.app/weather?latitude=${latitude}&longitude=${longitude}`
+    );
+
+    if (response.ok) {
+      const jsonData = await response.json();
+      instantiateUIChanges(jsonData);
+
+      return new Promise((resolve) => setTimeout(resolve, 1500));
+    }
+
+    throw new Error();
+  } catch (error) {
+    instantiate404Prompt("Location not found");
+    return Promise.reject(error);
+  }
+}
+
+async function getMatchingLocations(query) {
+  try {
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=10&language=en&format=json`
+    );
+
+    const jsonData = await response.json();
+    const location = jsonData["results"];
+    const matches = {};
+
+    if (location !== undefined) {
+      location.forEach((loc) => {
+        matches[
+          `${loc.name}, ${loc.admin1 === undefined ? loc.country : loc.admin1}`
+        ] = [loc.latitude, loc.longitude];
+      });
+    }
+
+    return matches;
+  } catch (error) {
+    return {};
+  }
+}
+
+export { getWeatherData, getMatchingLocations };
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -7,52 +7,52 @@ import {
   getMinTemperature,
   getTemperatureRange,
   getCeiledValue,
-  getWeatherConditionUsingCode
-} from "./weatherUtils"
+  getWeatherConditionUsingCode,
+} from "./weatherUtils";
 
 function updateStatisticsDashboard(weatherData) {
-  const userLocation = document.querySelector(".meta-wrapper__location p")
-  const dateMeta = document.querySelector(".meta-wrapper__location time")
-  const timeMeta = document.querySelector(".meta-wrapper__time time")
-  const temperature = document.querySelector(".current__temp")
-  const temperatureRange = document.querySelector(".range__min-to-max")
-  const weatherCondition = document.querySelector(".weather-condition__string")
+  const userLocation = document.querySelector(".meta-wrapper__location p");
+  const dateMeta = document.querySelector(".meta-wrapper__location time");
+  const timeMeta = document.querySelector(".meta-wrapper__time time");
+  const temperature = document.querySelector(".current__temp");
+  const temperatureRange = document.querySelector(".range__min-to-max");
+  const weatherCondition = document.querySelector(".weather-condition__string");
   const weatherIllustrationImage = document.querySelector(
-    ".stat-data__illustration"
-  )
+    ".stat-data__illustration",
+  );
   const statisticsDashboard = document.querySelector(
-    ".header-container__statistics"
-  )
+    ".header-container__statistics",
+  );
 
-  userLocation.textContent = getFormattedLocation(weatherData.location)
+  userLocation.textContent = getFormattedLocation(weatherData.location);
 
-  dateMeta.textContent = getFormattedDate(weatherData.location.localtime)
+  dateMeta.textContent = getFormattedDate(weatherData.location.localtime);
 
-  timeMeta.textContent = getFormattedTime(weatherData.location.localtime)
+  timeMeta.textContent = getFormattedTime(weatherData.location.localtime);
 
-  temperature.textContent = getMaxTemperature(weatherData.current, true)
+  temperature.textContent = getMaxTemperature(weatherData.current, true);
 
   temperatureRange.textContent = getTemperatureRange(
     temperature.textContent,
     weatherData.daily,
-    0
-  )
+    0,
+  );
 
   weatherCondition.textContent = getWeatherConditionUsingCode(
-    weatherData.current.condition.code
-  )
+    weatherData.current.condition.code,
+  );
 
   weatherIllustrationImage.src = getFormattedImage(
     weatherCondition.textContent,
     "svg",
-    weatherData.current
-  )
+    weatherData.current,
+  );
 
   statisticsDashboard.style.backgroundImage = `url("${getFormattedImage(
     weatherCondition.textContent,
     "png",
-    weatherData.current
-  )}")`
+    weatherData.current,
+  )}")`;
 }
 
 function updateGeneralStatistics(weatherData) {
@@ -61,33 +61,32 @@ function updateGeneralStatistics(weatherData) {
     rainProbability: `${weatherData.daily.precipitation_probability_max[0]}%`,
     windSpeed: `${getCeiledValue(weatherData.current.wind_kph)} Km/h`,
     humidity: `${weatherData.current.humidity}%`,
-    uvIndex: weatherData.current.uv
-  }
+    uvIndex: weatherData.current.uv,
+  };
 
-  const keysForStatValues = Object.keys(statValues)
+  const keysForStatValues = Object.keys(statValues);
 
   const generalStatisticsList = document.querySelector(
-    ".overview-section__list"
-  )
+    ".overview-section__list",
+  );
 
-  for (let i = 0; i < generalStatisticsList.children.length; i++) {
-    generalStatisticsList.children[i].children[1].textContent =
-      statValues[keysForStatValues[i]]
+  for (let i = 0; i < generalStatisticsList.children.length; i += 1) {
+    generalStatisticsList.children[i].children[1].textContent = statValues[keysForStatValues[i]];
   }
 }
 
 function updatePredictions(weatherData) {
-  const futurePredictions = weatherData.daily
-  const listWrapper = document.querySelector(".predictions__list")
+  const futurePredictions = weatherData.daily;
+  const listWrapper = document.querySelector(".predictions__list");
 
-  listWrapper.innerHTML = ""
+  listWrapper.innerHTML = "";
 
-  for (let i = 1; i < 6; i++) {
-    const listItem = document.createElement("li")
-    const currentDate = futurePredictions["time"][i]
-    const formattedDay = getFormattedDate(currentDate).split(",")[0]
-    const weatherCode = futurePredictions["weathercode"][i]
-    const weatherCondition = getWeatherConditionUsingCode(weatherCode)
+  for (let i = 1; i < 6; i += 1) {
+    const listItem = document.createElement("li");
+    const currentDate = futurePredictions.time[i];
+    const formattedDay = getFormattedDate(currentDate).split(",")[0];
+    const weatherCode = futurePredictions.weathercode[i];
+    const weatherCondition = getWeatherConditionUsingCode(weatherCode);
 
     listItem.innerHTML = `<span class="list__day">
                             ${formattedDay.substring(0, 3)}
@@ -96,9 +95,9 @@ function updatePredictions(weatherData) {
                               </span>
                             </span>
                             <img src="${getFormattedImage(
-                              weatherCondition,
-                              "svg"
-                            )}" alt="">
+    weatherCondition,
+    "svg",
+  )}" alt="">
 
                             <p class="list__stats">
                                 <span class="stats__weather">
@@ -109,62 +108,73 @@ function updatePredictions(weatherData) {
                                 <span class="list__temp-range">
                                 <span class="temp-range__sr-only">Temperature Range</span>
                                     ${getMaxTemperature(
-                                      futurePredictions,
-                                      false,
-                                      i
-                                    )}
+    futurePredictions,
+    false,
+    i,
+  )}
                                 <abbr title="to" class="sr-only">-</abbr>
                                 <span class="temp-range__to">
                                     ${getMinTemperature(futurePredictions, i)}
                                 </span>
                                 </span>
-                            </p>`
+                            </p>`;
 
-    listWrapper.appendChild(listItem)
+    listWrapper.appendChild(listItem);
   }
 }
 
 function instantiate404Prompt(error) {
-  const errorPrompt = document.querySelector(".main__error-prompter")
+  const errorPrompt = document.querySelector(".main__error-prompter");
   const errorConveyingText = errorPrompt.querySelector(
-    ".error-prompter__heading"
-  )
+    ".error-prompter__heading",
+  );
 
-  errorConveyingText.textContent = error
-  errorPrompt.classList.toggle("main__error-prompter--active")
+  errorConveyingText.textContent = error;
+  errorPrompt.classList.toggle("main__error-prompter--active");
 
   setTimeout(() => {
-    errorPrompt.classList.toggle("main__error-prompter--active")
-  }, 4000)
-}
-
-function activateIntroForm(listWrapper, state) {
-    const introFormSecon = listWrapper.parentElement.parentElement
-
-    if(introFormSecon.classList[0] !== "main__intro") return
-
-    state === "on"
-    ? introFormSecon.classList.add("main__intro--form-activated")
-    : (introFormSecon.className = "main__intro")
+    errorPrompt.classList.toggle("main__error-prompter--active");
+  }, 4000);
 }
 
 function deactivateDashboard(mainElement) {
-    const homeBtn = document.querySelector(".header-wrapper__header")
+  const homeBtn = document.querySelector(".header-wrapper__header");
 
-    homeBtn.addEventListener("click", () => {
-        mainElement.classList = ""
-    })
+  homeBtn.addEventListener("click", () => {
+    // eslint-disable-next-line no-param-reassign
+    mainElement.classList = "";
+  });
+}
+
+function activateDashboard() {
+  const mainElement = document.querySelector("main");
+
+  mainElement.classList.add("main--dashboard-active");
+
+  deactivateDashboard(mainElement);
+}
+
+function activateIntroForm(listWrapper, state) {
+  const introFormSection = listWrapper.parentElement.parentElement;
+
+  if (introFormSection.classList[0] !== "main__intro") return;
+
+  if (state === "on") {
+    introFormSection.classList.add("main__intro--form-activated");
+  } else {
+    introFormSection.className = "main__intro";
+  }
 }
 
 function instantiateUIChanges(jsonResponse) {
-    updateStatisticsDashboard(jsonResponse)
-    updateGeneralStatistics(jsonResponse)
-    updatePredictions(jsonResponse)
+  updateStatisticsDashboard(jsonResponse);
+  updateGeneralStatistics(jsonResponse);
+  updatePredictions(jsonResponse);
 }
 
 export {
-    instantiateUIChanges,
-    instantiate404Prompt,
-    activateDashboard,
-    activateIntroForm
-}
+  instantiateUIChanges,
+  instantiate404Prompt,
+  activateDashboard,
+  activateIntroForm,
+};
